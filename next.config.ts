bb15b98520543de8d485b4c4f1f844437495363f
Эльчин Кулиев <el.kuliyev@gmail.com>
2026-02-16 10:59:26 +0000

    замени изображение в AboutUS на https://motorsoil.com.ua/content/uploads

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'motorsoil.com.ua',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
